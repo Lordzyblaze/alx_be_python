4575e54604d9b0b4b3b75e160fa75928e62f4ebc
Adams Adjartey <melawzy88@gmail.com>
2024-07-03 19:18:43 +0000

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#Assign specific values to two variables, number1 and number2 
+number1 = 10
+number2 = 5
+
+# Perform addition, subtraction, and multiplication on these numbers.
+
+sum = number1 + number2
+sub = number2 - number1
+mul = number1 * number2
+
+# Print the results of these operations in a human-readable format.
+
+print ("Addition of 10 and 5 is", sum)
+print ("Substruction of 10 and 5 is", sub)
+print ("Multiplication of 10 and 5 is", mul)
