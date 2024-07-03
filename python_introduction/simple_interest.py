bdd694bb861d0073cf8f4615923c4cf4d2ef3ff4
Adams Adjartey <melawzy88@gmail.com>
2024-07-03 19:38:47 +0000

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#Assign specific values to 3 variables, Principal, rate,and time
+
+principal = 1000
+rate = 0.5
+time = 3
+
+#Calculate the simple interest using the formula provided and store the result in a variable named
+#interest 
+
+Interest = principal * rate * time
+
+#Print the calculated interest in a format: The simple interest is: [interest].
+print("Interest is", Interest)
